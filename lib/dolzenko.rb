939935cb33f2f219b94d08d4d408a1023b9bb546
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-04-27 15:14:11 +0400

    Add my Gists

--- lib/dolzenko.rb
+++ lib/dolzenko.rb
@@ -4,3 +4,7 @@ require "open-uri"
 
 require "active_support"
 require "facets"
+
+require "require_gist"
+require_gist "371861/abc6d24346864e5cb33b4eab330569565b1dd8c2/shell_out.rb", "72b19c8955f87f9b408a92a77440cda987b1a01f" # http://gist.github.com/371861
+require_gist "375386/642be35e02a09b7dc5736f462ea1d8368864ffa8/error_print.rb", "12f893f4abd1f0ea19d198f6ff1ac66d8b8675ea" # http://gist.github.com/375386
