8446d9bbda9d08e252e708cc4712ce76a0b67dbf
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-04-27 15:17:04 +0400

    Fix push_and_release.rb

--- push_and_release.rb
+++ push_and_release.rb
@@ -2,14 +2,18 @@ require "yaml"
 
 GEM_NAME = "dolzenko"
 
-current_version = YAML.load(`gem specification #{ GEM_NAME } -r`)["version"] rescue Gem::Version.new("0.0.0")
+current_version = YAML.load(`gem specification #{ GEM_NAME } -r`)["version"] || Gem::Version.new("0.0.0")
 new_version = (current_version.segments[0..-2] + [current_version.segments[-1].succ]).join(".")
 ENV["GEM_VERSION"] = new_version
 
 puts "Releasing #{ GEM_NAME } #{ new_version }"
 
-system "gem build dolzenko.gemspec"
+system "gem build #{ GEM_NAME }.gemspec"
 
-system "gem push dolzenko-#{ new_version }"
+system "gem push #{ GEM_NAME }-#{ new_version }.gem"
+
+File.delete("#{ GEM_NAME }-#{ new_version }.gem")
 
 system "git push"
+
+system "gem install #{ GEM_NAME } --version=#{ new_version }"
