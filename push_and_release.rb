69b808bdd122bd7790e6fb685c22a0743e1102a6
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-04-28 10:53:19 +0400

    Again 'Make sure new version is installed directly after push'

--- push_and_release.rb
+++ push_and_release.rb
@@ -12,8 +12,9 @@ system "gem build #{ GEM_NAME }.gemspec --verbose"
 
 system "gem push #{ GEM_NAME }-#{ new_version }.gem --verbose"
 
+system "gem install #{ GEM_NAME } --version=#{ new_version } --local --verbose"
+
 File.delete("#{ GEM_NAME }-#{ new_version }.gem")
 
 system "git push"
 
-system "gem install #{ GEM_NAME } --version=#{ new_version } --remote --update-sources --verbose"
